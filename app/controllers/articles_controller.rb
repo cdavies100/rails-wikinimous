6c1106b825592ff7fb754d136d961450c1a7c74d
Chiara Davies <chiaradavies01@outlook.com>
2017-11-07 16:03:14 +0000

    added comments and solution for batch 103

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,41 +1,65 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-  def index         # GET /restaurants
+
+  def index # GET /restaurants
     @articles = Article.all
   end
 
-  def show
+  def show # GET /articles/1
+    # don't need this line when before_action is set_article
     @article = Article.find(params[:id])
   end
 
-  def new
+  def new # GET /articles/new
+    # don't need the () after .new
     @article = Article.new()
   end
 
-  def create
+  def edit # GET /articles/1/edit
+    # don't need this line when before_action is set_article
+    @article = Article.find(params[:id])
+  end
+
+  def create # POST /articles
     new_article = Article.new(articles_params)
     new_article.save
     redirect_to article_path(new_article)
+    # batch 103 soltion has this if statement in the method after Article.new line:
+    # if @article.save
+    #   redirect_to @article, notice: 'Article was successfully created.'
+    # else
+    #   render :new
+    # end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  def update
+  def update # PATCH/PUT /articles/1
+    # don't need first line when before_action is set_article
     @article = Article.find(params[:id])
     @article.update(articles_params)
     redirect_to article_path
+    # batch 103 has just this in the update method:
+    # if @article.update(article_params)
+    #   redirect_to @article, notice: 'Article was successfully updated.'
+    # else
+    #   render :edit
+    # end
   end
 
-  def destroy
+  def destroy # DELETE /articles/1
+    # don't need first line when before_action is set_article
     article_to_delete = Article.find(params[:id])
     article_to_delete.destroy
     redirect_to articles_path
   end
 
   private
+  # Use callbacks to share common setup or constraints
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
+  # Only allow a trusted parameter "white list" through
   def articles_params
     params.require(:article).permit(:title, :content)
   end
